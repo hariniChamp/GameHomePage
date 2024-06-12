1e8208ef06bfba6a48ca9b0fb1ac051ecaadf450
Harini <harinichamp02@gmail.com>
2024-06-12 13:01:54 -0500

    Initial Commit

---
+++ GameHomePage/GameHomePageApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameHomePageApp.swift
+//  GameHomePage
+//
+//  Created by Scholar on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GameHomePageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
